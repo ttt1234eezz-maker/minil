3e4646270b75f2cdb62beba7b9fb32d246f4f754
ttt1234eezz-maker <ttt1234eezz@gmail.com>
2026-06-09 18:16:19 +0300

    Update system call definitions for multiple architectures

--- arch.h
+++ arch.h
@@ -2,30 +2,23 @@
 #define ARCH_H
 
 #if defined(__x86_64__)
-    #define SYS_WRITE 1
+    #define SYS_READ  0; #define SYS_WRITE 1; #define SYS_OPEN  2; #define SYS_EXIT  60
     #define ASM_SYSCALL "syscall"
-    #define ARG0 %rdi
-    #define ARG1 %rsi
-    #define ARG2 %rdx
+    #define ARG0 %rdi; #define ARG1 %rsi; #define ARG2 %rdx
 #elif defined(__i386__)
-    #define SYS_WRITE 4
+    #define SYS_READ  3; #define SYS_WRITE 4; #define SYS_OPEN  5; #define SYS_EXIT  1
     #define ASM_SYSCALL "int $0x80"
-    #define ARG0 %ebx
-    #define ARG1 %ecx
-    #define ARG2 %edx
+    #define ARG0 %ebx; #define ARG1 %ecx; #define ARG2 %edx
 #elif defined(__aarch64__)
-    #define SYS_WRITE 64
+    #define SYS_READ  63; #define SYS_WRITE 64; #define SYS_OPEN  56; #define SYS_EXIT  93
     #define ASM_SYSCALL "svc #0"
-    #define ARG0 x0
-    #define ARG1 x1
-    #define ARG2 x2
+    #define AT_FDCWD -100
+    #define ARG0 x0; #define ARG1 x1; #define ARG2 x2
 #elif defined(__riscv)
-    #define SYS_WRITE 64
-    #define SYS_EXIT  93
+    #define SYS_READ  63; #define SYS_WRITE 64; #define SYS_OPEN  56; #define SYS_EXIT  93
     #define ASM_SYSCALL "ecall"
-    #define ARG0 a0
-    #define ARG1 a1
-    #define ARG2 a2
+    #define AT_FDCWD -100
+    #define ARG0 a0; #define ARG1 a1; #define ARG2 a2
 #endif
 
 #endif
