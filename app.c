27bb80fbc20bc38ff3ea5d9ddaa66e5aa5420eb1
ttt1234eezz-maker <ttt1234eezz@gmail.com>
2026-06-09 20:26:46 +0300

    Simplify main function to print 'Hello, world!'
    Removed architecture-specific greeting and exit call.

--- app.c
+++ app.c
@@ -1,14 +1,8 @@
 /* This software is dedicated to the public domain under CC0 1.0 Universal. */
 /* See LICENCE.md for full legal text. */
 void println(const char *);
-void _exit(int);
 
 int main(void)
 {
-#if defined(__x86_64__)
-    println("Hello from 64 bit minil");
-#else
-    println("Hello from 32 bit minil");
-#endif
-    _exit(0);
+    println("Hello, world!")
 }
