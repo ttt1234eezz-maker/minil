5e9f7895dd9938a4c17c9e66a2395ff7c2d6580d
ttt1234eezz-maker <ttt1234eezz@gmail.com>
2026-06-09 18:01:32 +0300

    Remove SYS_EXIT definition for x86_64 and aarch64

--- arch.h
+++ arch.h
@@ -3,21 +3,29 @@
 
 #if defined(__x86_64__)
     #define SYS_WRITE 1
-    #define SYS_EXIT  60
     #define ASM_SYSCALL "syscall"
     #define ARG0 %rdi
     #define ARG1 %rsi
     #define ARG2 %rdx
+#elif defined(__i386__)
+    #define SYS_WRITE 4
+    #define ASM_SYSCALL "int $0x80"
+    #define ARG0 %ebx
+    #define ARG1 %ecx
+    #define ARG2 %edx
 #elif defined(__aarch64__)
     #define SYS_WRITE 64
-    #define SYS_EXIT  93
     #define ASM_SYSCALL "svc #0"
     #define ARG0 x0
     #define ARG1 x1
+    #define ARG2 x2
 #elif defined(__riscv)
     #define SYS_WRITE 64
     #define SYS_EXIT  93
     #define ASM_SYSCALL "ecall"
+    #define ARG0 a0
+    #define ARG1 a1
+    #define ARG2 a2
 #endif
 
 #endif
